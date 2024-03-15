2629cb0f69f8359e45d99b074e6b7e86b52ea0af
Zhongzhi Yu <zyu401@eic-gt-gpu6.cc.gatech.edu>
2024-03-15 06:53:28 -0400

    experiment

---
+++ experiment/infer.py
@@ -0,0 +1,11 @@
+"""
+this file tries to test the accuracy of medusa-head predicting
+"""
+from medusa.model.medusa_model import MedusaModel
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    pass
